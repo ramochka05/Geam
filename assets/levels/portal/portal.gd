1de645ec3804b7c616eb870ab6867f87a86ff330
VladSelyanskiy <vlad.selyanskiy@mail.ru>
2026-06-22 21:52:13 +0300

    fix: delete extra files

--- assets/levels/portal/portal.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-
-@export var scene: String
-
-func _on_body_entered(body: Node2D) -> void:
-	var NewScene = load("res://scene/"+scene+".tscn")
-	get_tree().change_scene_to_packed(NewScene)
--- portal/portal.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-
-@export var scene: String
-
-func _on_body_entered(body: Node2D) -> void:
-	var NewScene = load("res://assets/levels/hall/hall.tscn")
-	get_tree().change_scene_to_packed(NewScene)
